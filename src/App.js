f6d5750f773c4495f879b122a67b6e81f18abd26
Menuka Pinto <instafeed9999@gmail.com>
2021-08-26 15:00:38 +0530

    Added all the components done locally

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import Button from '@material-ui/core/Button';
+// import SideMenu from "./components/SideMenu";
+import NavBar from './components/NavBar';
+// import InStockSection from './components/InStockSection';
+// import UploadButtons from './components/t';
+// import ProductTable from './components/ProductTable';
+import MTable from './components/t';
+import LineSection from './components/LineSection';
+// import { makeStyles } from '@material-ui/styles';
+// import NavSideBar from './components/NavSideBar';
+import Chart from './components/Chart';
+import StyleSheet from './components/StyleSheet'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      <NavBar/>
+      {/* <NavSideBar/> */}
+      
+      <LineSection/>
+      {/* <StyleSheet/> */}
+      <MTable/>
+      <Chart/>
     </div>
   );
 }
