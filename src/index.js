0e47bd0162cb373efcb71bafbcd6530ff256b8bf
Damian-Cntr1 <damiansdemel@gmail.com>
2021-09-20 21:24:37 +0530

    second commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import ReactRout from "./components/ReactRout"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ReactRout />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
