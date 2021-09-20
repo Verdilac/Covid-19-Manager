d1ea56eed733d06d0f6287ef0feb5f77ccb1a198
Menuka Pinto <instafeed9999@gmail.com>
2021-09-20 20:32:33 +0530

    Updated firebase config

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 
 // Firebase Configuration with remote database
 const firebaseConfig = {
-    apiKey: "AIzaSyCv2nqmAyxQYoYMyuM1goUKCVSegH_fwaA",
-    authDomain: "hospital-inventory-managment.firebaseapp.com",
-    databaseURL: "https://hospital-inventory-managment-default-rtdb.firebaseio.com",
-    projectId: "hospital-inventory-managment",
-    storageBucket: "hospital-inventory-managment.appspot.com",
-    messagingSenderId: "442613914850",
-    appId: "1:442613914850:web:9bd16199f94a28c8c492bb",
-    measurementId: "G-S4FYCRN7CK"
+    apiKey: "AIzaSyBn9O1pBo1EDW_bnVylJKs99qv6n6n6kl4",
+    authDomain: "itp-covid-manager.firebaseapp.com",
+    databaseURL: "https://itp-covid-manager-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "itp-covid-manager",
+    storageBucket: "itp-covid-manager.appspot.com",
+    messagingSenderId: "983496885829",
+    appId: "1:983496885829:web:79b7bbfda6540bb60d852c",
+    measurementId: "G-EYKSLEWLXD"
 }
 
 firebase.initializeApp(firebaseConfig);
