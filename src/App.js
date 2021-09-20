ddd0ca8299c9f3a5fb88f336c88949392add024a
Damian-Cntr1 <damiansdemel@gmail.com>
2021-09-20 21:31:53 +0530

    second commit#

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard'
+
+function App (){
+    return (
+          <div>
+                    <Dashboard/>
+          </div>
+    );
+}
+
+export default App;
