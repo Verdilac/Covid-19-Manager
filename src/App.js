42fceb09557700d49cc1c8ef22aa7cb0e1d5ac85
Menuka Pinto <instafeed9999@gmail.com>
2021-09-20 22:47:49 +0530

    back to normal

--- src/App.js
+++ src/App.js
@@ -1,2 +1,20 @@
+import React from 'react';
+// import './App.css';
+
+import InventoryDashboard from './Components/InventoryFunctions/InventoryDashboard/InventoryDashboard'
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <InventoryDashboard />
+      
+    </div>
+    // Pinto - Inventory_Function
+  );
+}
+
+export default App;
 
 
