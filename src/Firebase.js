a42ea7a3c41dfe951fe39a729edb72b72e1028db
Charuka Kaluarachchi <70794286+cdkaluarachchi@users.noreply.github.com>
2021-09-20 22:37:29 +0530

    test

---
+++ src/Firebase.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase';
+
+const settings = {timestampsInSnapshots: true};
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+  apiKey: "AIzaSyBn9O1pBo1EDW_bnVylJKs99qv6n6n6kl4",
+
+  authDomain: "itp-covid-manager.firebaseapp.com",
+
+  databaseURL: "https://itp-covid-manager-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+  projectId: "itp-covid-manager",
+
+  storageBucket: "itp-covid-manager.appspot.com",
+
+  messagingSenderId: "983496885829",
+
+  appId: "1:983496885829:web:79b7bbfda6540bb60d852c",
+
+  measurementId: "G-EYKSLEWLXD"
+};
+
+
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;
