c3407857e13471dba24fea8a016906f03bbe7125
Praveen Peiris <praveen.peiris.pp@gmail.com>
2021-09-21 12:59:49 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
+import DocProfiles from './DocProfiles';
+import DocTables from './DocTables';
+import AssignDoc from './Components/AssignDoc';
+import DocTable from './Components/DocTable';
+import Update from './Components/Update';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Router>
+      <Switch>
+        <Route exact path="/register" component={AssignDoc} />
+        <Route exact path="/" component={DocTable} />
+      </Switch>
+    </Router>
+    <DocTables />
+    
+    </div> //Praveen
   );
 }
 
