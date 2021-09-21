4dab823ee1747920c01124d84379d7979842b471
Tivain <70811407+Tivain-W@users.noreply.github.com>
2021-09-21 11:51:47 +0530

    editted index.js & index.html

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Edit from './components/hospital-vacancy-management/Edit';
-import Create from './components/hospital-vacancy-management/Create';
-import Show from './components/hospital-vacancy-management/Show';
 
 ReactDOM.render(
-  <Router>
-      <div>
-        <Route exact path='/' component={App} />
-        <Route path='/edit/:id' component={Edit} />
-        <Route path='/create' component={Create} />
-        <Route path='/show/:id' component={Show} />
-      </div>
-  </Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
