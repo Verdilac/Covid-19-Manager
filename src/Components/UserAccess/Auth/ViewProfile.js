8bb51a1828b96ad5611ab59ea90ba14175b29068
Damian-Cntr1 <damiansdemel@gmail.com>
2021-10-11 19:35:38 +0530

    UserAccess function

---
+++ src/Components/UserAccess/Auth/ViewProfile.js
@@ -0,0 +1,48 @@
+import React, { useState, Component } from "react"
+import { Card, Button, Alert } from "react-bootstrap"
+import { useAuth } from "../Contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+import firebase from '../firebase';
+
+import TextField from '@material-ui/core/TextField';
+//import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import SaveIcon from '@material-ui/icons/Save';
+
+
+export default function ViewProfile() {
+  const [error, setError] = useState("")
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+
+  async function handleLogout() {
+    setError("")
+
+    try {
+      await logout()
+      history.push("/login")
+    } catch {
+      setError("Failed to log out")
+    }
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  )
+}
